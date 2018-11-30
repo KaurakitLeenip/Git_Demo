63683986d22fdf66f2f24c68e1d2cc602063622b
Kaurakit <kaurakit@gmail.com>
2018-11-30 15:55:01 +0700

    asdf

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 def main():
     print('asdf')
-    
-if __name__ == "main":
+    s = ''
+    for u in range(16, 1, -2):
+        for i in range(u, 16, 1):
+            s += '*'
+        print(s.center(100, ' '))
+        s = ''
+    s = ''
+    for e in range(1, 16, 2):
+        for r in range(e, 16, 1):
+            s += '*'
+        print(s.center(100, ' '))
+        s = ''
+
+if __name__ == "__main__":
     main()
